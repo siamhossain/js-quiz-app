431eb927ff59a14713df44c6baacc4124ad8c093
Siam Hossain <siam.hossain1994@gmail.com>
2020-05-02 18:23:35 +0600

    Add more questions and complete

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
@@ -82,5 +83,30 @@ const questions = [
             { text: '4', correct: true },
             { text: '22', correct: false }
         ]
-    }
+    },
+    {
+        question: 'Who is the best YouTuber?',
+        answers: [
+          { text: 'Web Dev Simplified', correct: true },
+          { text: 'Traversy Media', correct: true },
+          { text: 'Dev Ed', correct: true },
+          { text: 'Fun Fun Function', correct: true }
+        ]
+      },
+      {
+        question: 'Is web development fun?',
+        answers: [
+          { text: 'Kinda', correct: false },
+          { text: 'YES!!!', correct: true },
+          { text: 'Um no', correct: false },
+          { text: 'IDK', correct: false }
+        ]
+      },
+      {
+        question: 'What is 4 * 2?',
+        answers: [
+          { text: '6', correct: false },
+          { text: '8', correct: true }
+        ]
+      }
 ]
